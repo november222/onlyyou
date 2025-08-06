09a941ae3435549e2b3ec2751dfd899ccea33cce
november222 <115559177+november222@users.noreply.github.com>
2025-08-06 23:40:42 +0900

    Hide default React Navigation header in history layout

--- app/history/_layout.tsx
+++ app/history/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from 'expo-router/stack';
 
 export default function HistoryLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen 
         name="index" 
         options={{ 
