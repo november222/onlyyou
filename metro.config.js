0ad5dbbc0173a2db4bcab3348969667bca226255
november222 <115559177+november222@users.noreply.github.com>
2025-08-19 22:30:36 +0900

    Fix Metro resolver for config directory

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,13 @@
 // metro.config.js
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
+
+// Add project root to resolver paths
+config.resolver.nodeModulesPaths = [
+  path.resolve(__dirname, 'node_modules'),
+  path.resolve(__dirname)
+];
 
 module.exports = config;
